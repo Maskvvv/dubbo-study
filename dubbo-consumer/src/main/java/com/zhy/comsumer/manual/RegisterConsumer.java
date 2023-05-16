cd359dd180d4a2007cc004902efe92c023a772dd
zhouhongyin <1429855087@qq.com>
2023-05-16 17:39:01 +0800

    手动注册 consumer

---
+++ dubbo-consumer/src/main/java/com/zhy/comsumer/manual/RegisterConsumer.java
@@ -0,0 +1,34 @@
+package com.zhy.comsumer.manual;
+
+import com.zhy.spi.UserService;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+
+import java.io.IOException;
+
+/**
+ * @author zhouhongyin
+ * @since 2023/5/16 17:21
+ */
+public class RegisterConsumer {
+
+    public static void main(String[] args) throws IOException {
+        ApplicationConfig applicationConfig = new ApplicationConfig("dubbo-provider");
+
+        RegistryConfig registryConfig = new RegistryConfig("zookeeper://zhouhongyin.top:21810");
+
+        ReferenceConfig<UserService> referenceConfig = new ReferenceConfig<>();
+        referenceConfig.setApplication(applicationConfig);
+        referenceConfig.setRegistry(registryConfig);
+        referenceConfig.setInterface(UserService.class);
+
+        UserService userService = referenceConfig.get();
+        String user = userService.getUser("hahaha");
+
+        System.in.read();
+
+    }
+
+}
